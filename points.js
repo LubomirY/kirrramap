8ae2f29a49a4f5370bd6542d50082fbe603bf834
LubomirY <2lubomir8@gmail.com>
2025-10-01 21:12:58 +0200

    Add files via upload

---
+++ points.js
@@ -0,0 +1,157 @@
+const points = [
+//moss grounds
+  {
+    coords: [989, 696],
+    title: "Скамейка",
+    img: "screenshots/benches/old_chapel_bench.png",
+    desc: "Старая капелла\n",
+    iconUrl: "icons/bench.png"
+  },
+  {
+    coords: [917, 851],
+    title: "Скамейка",
+    img: "screenshots/benches/bone_bottom_bench.png",
+    desc: "Костяная низина\n",
+    iconUrl: "icons/bench.png"
+  },
+  {
+    coords: [917, 885],
+    title: "Звонкий путь",
+    img: "screenshots/bellway/bone_bottom_bellway.png",
+    desc: "Средство быстрого передвижения",
+    iconUrl: "icons/bellway.png"
+  },
+  {
+    coords: [917, 917],
+    title: "Пебб",
+    img: "screenshots/npc/bone_bottom_shop.png",
+    desc: "Торговец костяной низины\n\nПродаёт: магнит, осколок маски, простой ключ, руда, чётки",
+    iconUrl: "icons/shop.png"
+  },
+
+
+//the marrow
+  {
+    coords: [943, 1188],
+    title: "Скамейка",
+    img: "screenshots/benches/marrow_1_bench.png",
+    desc: "Костяк (врата)\n",
+    iconUrl: "icons/bench.png"
+  },
+  {
+    coords: [815, 976],
+    title: "Скамейка",
+    img: "screenshots/benches/marrow_2_bench.png",
+    desc: "Костяк (Шакра)\n",
+    iconUrl: "icons/bench.png"
+  },
+  {
+    coords: [761, 1035],
+    title: "Скамейка",
+    img: "screenshots/benches/moss_druid_bench.png",
+    desc: "Мшистый друид\n",
+    iconUrl: "icons/bench.png"
+  },
+  {
+    coords: [792, 1157],
+    title: "Звонкий путь",
+    img: "screenshots/bosses/bell_beast_boss.png",
+    desc: "Средство быстрого передвижения",
+    iconUrl: "icons/bellway.png"
+  },
+  {
+    coords: [792, 1226],
+    title: "Скамейка",
+    img: "screenshots/benches/marrow_bell_bench.png",
+    desc: "Костяк (Колокол)",
+    iconUrl: "icons/bench.png"
+  },
+  {
+    coords: [880, 1317],
+    title: "Скамейка",
+    img: "screenshots/benches/marrow_flee_bench.png",
+    desc: "Блошиный караван\n",
+    iconUrl: "icons/bench.png"
+  },
+  {
+    coords: [880, 1333],
+    title: "Блошиный караван",
+    img: "screenshots/npc/marrow_flee_caravan.png",
+    desc: "Помогите найти и освободить потерявшихся блох\nСпасено: 0/5",
+    iconUrl: "icons/caravan.png"
+  },
+  {
+    coords: [870, 1608],
+    title: "Скамейка",
+    img: "screenshots/benches/marrow_jail_bench.png",
+    desc: "Костяк (тюрьма)",
+    iconUrl: "icons/bench.png"
+  },
+
+
+// deap docks
+
+
+// secrets
+  {
+    coords: [806, 718],
+    title: "Неосмотренный проход",
+    desc: "",
+    iconUrl: "icons/missed_area.png"
+  },
+
+  {
+    coords: [1030, 897],
+    title: "Дверь в виде ткача",
+    desc: "Перед ней расположен музыкальный инструмент со струнами из шёлка",
+    iconUrl: "icons/needolin_door.png"
+  },
+
+  {
+    coords: [890, 950],
+    title: "Неосмотренный проход",
+    desc: "",
+    iconUrl: "icons/missed_upgrade.png"
+  },
+  {
+    coords: [699, 1245],
+    title: "Неосмотренная комната",
+    desc: "",
+    iconUrl: "icons/missed_misc.png"
+  },
+
+  {
+    coords: [713, 1294],
+    title: "Необходима клешня",
+    desc: "",
+    iconUrl: "icons/claw_tool.png"
+  },
+  {
+    coords: [875, 1429],
+    title: "Проход открываеться с другой стороны",
+    desc: "",
+    iconUrl: "icons/left_area.png"
+  },
+  {
+    coords: [909, 1534],
+    title: "Необходима клешня",
+    desc: "",
+    iconUrl: "icons/claw_area.png"
+  },
+
+  {
+    coords: [942, 1777],//949, 1788
+    title: "Неосмотренный пороход",
+    desc: "",
+    iconUrl: "icons/missed_area.png"
+  },
+
+
+//other
+  {
+    coords: [836, 1679],
+    title: "Скарр-страж",
+    desc: "Минибосс",
+    iconUrl: "icons/hunt.png"
+  },
+];
